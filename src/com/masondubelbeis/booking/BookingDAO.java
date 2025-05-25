bb890de6969288923dfde2037d54cc9b8bae91d5
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2025-05-25 16:06:40 -0500

    edited setBookings and return type on getBookings

--- src/com/masondubelbeis/booking/BookingDAO.java
+++ src/com/masondubelbeis/booking/BookingDAO.java
@@ -1,4 +1,15 @@
 package com.masondubelbeis.booking;
 
+
 public class BookingDAO {
+    private static Booking[] bookings = new Booking[3];
+
+
+    public static Booking[] getBookings() {
+        return bookings;
+    }
+
+    public static void setBookings(Booking booking) {
+        bookings[0] = booking;
+    }
 }
