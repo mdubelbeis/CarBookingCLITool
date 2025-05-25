5ab4b411b96cc9f662655a9cb2681d359221f335
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2025-05-25 14:22:14 -0500

    implemented UserDAO

--- src/com/masondubelbeis/user/UserDAO.java
+++ src/com/masondubelbeis/user/UserDAO.java
@@ -1,4 +1,17 @@
 package com.masondubelbeis.user;
 
 public class UserDAO {
+    private static String[] users = {};
+
+    public UserDAO() {
+
+    }
+
+    public static String[] getUsers() {
+        return users;
+    }
+
+    public static void setUsers(String[] users) {
+        UserDAO.users = users;
+    }
 }
