f9551e3420b88ae81daa4401e05a5c6908085571
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2025-05-25 11:48:20 -0500

    implemented Car class

--- src/com/masondubelbeis/car/Car.java
+++ src/com/masondubelbeis/car/Car.java
@@ -1,4 +1,49 @@
 package com.masondubelbeis.car;
 
+import java.math.BigDecimal;
+
 public class Car {
+    private String regNumber;
+    private BigDecimal rentalPricePerDay;
+    private String brand;
+    private boolean isElectric;
+
+    public Car(String regNumber, BigDecimal rentalPricePerDay, String brand, boolean isElectric) {
+        this.regNumber = regNumber;
+        this.rentalPricePerDay = rentalPricePerDay;
+        this.brand = brand;
+        this.isElectric = isElectric;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public BigDecimal getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    public void setRentalPricePerDay(BigDecimal rentalPricePerDay) {
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
+    }
 }
