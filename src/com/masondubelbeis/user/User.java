f93ada707a824fb3d724885bde1c376fa915e27c
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2025-05-25 10:13:22 -0500

    implemented User class

--- src/com/masondubelbeis/user/User.java
+++ src/com/masondubelbeis/user/User.java
@@ -1,4 +1,49 @@
 package com.masondubelbeis.user;
 
+import java.util.UUID;
+
 public class User {
+    private static UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(UUID id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public static void setId() {
+        id = UUID.randomUUID();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
